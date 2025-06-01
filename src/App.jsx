fad201dfdd37c903b8de0280b47f7430384afa88
Julio Ucharima <julio_ucharima@usmp.pe>
2025-06-01 14:54:17 -0500

    agregar estructura inicial

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { AppTheme } from "./config/theme/AppTheme";
+import { AppRouter } from "./routes/AppRouter";
+
+function App() {
+  return (
+    <>
+      <AppTheme>
+        <AppRouter />
+      </AppTheme>
+    </>
+  );
+}
+
+export default App;
---
+++ src/modules/auth/pages/LoginPage.jsx
@@ -0,0 +1,46 @@
+import { AuthLayout } from "../layout/AuthLayout";
+import { Button, Grid } from "@mui/material";
+
+const initialState = {
+  email: "",
+  password: "",
+};
+
+export const LoginPage = () => {
+  const onSubmit = () => {
+    console.log("Inicio de sesión exitoso", initialState);
+  };
+
+  return (
+    <AuthLayout title={"Inicio de Sesión"}>
+      <form onSubmit={onSubmit}>
+        <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+          {/* Error Message */}
+          {/* <Grid item xs={12} display={errorMessage ? "" : "none"}>
+              <Alert severity="error">{errorMessage}</Alert>
+            </Grid> */}
+
+          <Button
+            fullWidth
+            type="submit"
+            variant="contained"
+            sx={{
+              textTransform: "none",
+              display: "flex",
+              alignItems: "center",
+              gap: 1,
+            }}
+          >
+            Iniciar Sesión con Google
+            <img
+              src="/google.svg"
+              alt="google-logo"
+              width={20}
+              style={{ marginRight: "8px" }}
+            />
+          </Button>
+        </Grid>
+      </form>
+    </AuthLayout>
+  );
+};
